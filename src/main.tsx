9de6db04a720cff4478a913b9e9210b564bb1295
Yehia Hassan <yeh.ia@icloud.com>
2025-06-08 05:29:20 +0300

    Responsiveness update

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import './responsive-fixes.css'
+import './components/CreditHoursInputFix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
